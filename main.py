bb937c3c93cdc6504113ceeb5e2cea65cab59812
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-11-27 21:41:17 -0500

    prettytable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from prettytable import PrettyTable
 
 #Lista para almacenar los experimentos
 listaDeExperimentos = [
@@ -24,6 +25,7 @@ def agregar_experimento():
 #Funcion para eliminar un experimento
 def eliminar_experimento():
     visulizar_experimentos()
+    
     try:
         indice = int(input("Ingrese el indice del experimento a eliminar: ")) - 1
         if 0 <= indice < len(listaDeExperimentos):
@@ -36,12 +38,23 @@ def eliminar_experimento():
 
 #Funcion para visualizar mis experimentos
 def visulizar_experimentos():
+    
+    tablaExperimentos = PrettyTable()
+    
+    tablaExperimentos.field_names = ["id", "Nombre", "Fecha", "Tipo","Resultados"]
+    for i, experimento in enumerate(listaDeExperimentos, start=1):
+        tablaExperimentos.add_row([i, experimento[0], experimento[1], experimento[2],experimento[3]], divider=True)
+        
+    print(tablaExperimentos)
+    
+    """
     if not listaDeExperimentos:
         print("\033[1;33m"+"No existen experimentos registrados"+'\033[0;m')
         return
     print("==== Experimentos ====")
     for i, experimento in enumerate(listaDeExperimentos, start=1):
         print(f"{i}. Nombre: {experimento[0]}, Fecha: {experimento[1]}, Tipo: {experimento[2]}, Resultados: {experimento[3]}")
+    """
 
 #Funcion para calular estadisticas
 def calcular_estadisticas():
@@ -107,6 +120,22 @@ def generar_informe():
 
 #Funcion para mostrar el menu
 def mostar_menu():
+    
+    
+    tablaMenu = PrettyTable()
+    tablaMenu.align = "l"
+    tablaMenu.field_names = ["Menu Principal"]
+    tablaMenu.add_row(["1. Agregar experimento"])
+    tablaMenu.add_row(["2. Visualizar experimentos"])
+    tablaMenu.add_row(["3. Eliminar experimentos"])
+    tablaMenu.add_row(["4. Calcular estadisticas"])   
+    tablaMenu.add_row(["5. Comparar experimentos"])
+    tablaMenu.add_row(["6. Generar informe"])
+    tablaMenu.add_row(["7. Salir"])
+        
+    print(tablaMenu)
+
+    """
     print("===== Menu Principal =====")
     print("==========================")
     print("1. Agregar experimento")
@@ -116,11 +145,13 @@ def mostar_menu():
     print("5. Comparar experimentos")
     print("6. Generar informe")
     print("7. Salir")
-
+    
+    """
+    
 #Funcion principal
 def main():
     while True:
-        print("==========================")
+
         mostar_menu()
         try:
             opcion = int(input("Ingrese una opcion: "))
